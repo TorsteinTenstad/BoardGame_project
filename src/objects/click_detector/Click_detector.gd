70e4b86c55b091239e964d4e5f2dad970b358f78
TorsteinTenstad <52585723+TorsteinTenstad@users.noreply.github.com>
2021-03-25 13:49:14 +0100

    only top Click_detector triggers on mouse input

--- src/objects/click_detector/Click_detector.gd
+++ src/objects/click_detector/Click_detector.gd
@@ -12,11 +12,10 @@ func _ready():
 	scale = size
 
 
-func _on_Click_detector_input_event(viewport, event, shape_idx):
-	get_tree().set_input_as_handled()
-	if event is InputEventMouseButton:
-		if event.button_index in trigger_on:
-#			var shapes = get_world_2d().direct_space_state.intersect_point(get_global_mouse_position(), 32, [], 0x7FFFFFFF, true, true) # The last 'true' enables Area2D intersections, previous four values are all defaults
-#			print(shapes[0]['rid'].get_id(), "   ", get_rid().get_id())
-#			if shapes[0]['rid'].get_id() == get_rid().get_id():
-			emit_signal('clicked', event.button_index, event.pressed)
+func _input(event):
+	var local_mouse_pos = get_global_mouse_position() - global_position
+	if abs(local_mouse_pos.x) <= size.x and abs(local_mouse_pos.y) <= size.y:
+		if event is InputEventMouseButton:
+			if event.button_index in trigger_on:
+				emit_signal('clicked', event.button_index, event.pressed)
+				get_tree().set_input_as_handled()
